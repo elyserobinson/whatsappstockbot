dbf0a57119f2fbd5e19650ba4b6ce6e1211ad544
Elyse Y. Robinson <72902158+elyserobinson@users.noreply.github.com>
2020-12-17 21:59:14 -0600

    Upload project files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'rack'
+gem 'json'
+gem 'grape'
+gem 'thin'
+gem '3scale_client'
---
+++ marketstack.py
@@ -0,0 +1,27 @@
+# MARKETSTACK_KEY
+
+#1 Explore the marketstack docs and find the relevant url to use to call data
+#2 Integrate API
+
+# imports
+import os
+import requests
+import json
+
+API_KEY = os.environ.get("MARKETSTACK_KEY")
+BASE_URL = 'http://api.marketstack.com/v1/'
+
+def get_stock_price(stock_symbol):
+    params = {
+        'access_key': API_KEY
+    }
+    end_point = ''.join([BASE_URL, "tickers/", stock_symbol, "/intraday/latest"])
+    api_result = requests.get(end_point, params)
+    print(api_result)
+    json_result = json.loads(api_result.text)
+    return {
+        "last_price": json_result["last"]
+    }
+
+result = get_stock_price("TSLA")
+print(result)
